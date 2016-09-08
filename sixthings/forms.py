e8fdd57b2a00669892605e56adb312642c1ace74
Phillip Marshall <pmarshall@agrimgt.com>
2016-09-07 20:19:06 -0700

    implemented some baisc security checks, user is automatically set when creating new things

---
+++ sixthings/forms.py
@@ -0,0 +1,22 @@
+from django.forms import ModelForm
+from .models import Thing
+
+# Create the form class.
+class ThingCreateForm(ModelForm):
+    class Meta:
+        model = Thing
+        fields = [
+            'date',
+            'text',
+        ]
+
+    def __init__(self, user, *args, **kwargs):
+        super(ThingCreateForm, self).__init__(*args, **kwargs)
+        self._user = user
+
+    def save(self, commit=True, **kwargs):
+        m = super(ThingCreateView, self).save(commit=False, **kwargs)
+        self.instance.user = self._user
+        if commit:
+            m.save()
+        return m
--- sixthings/views.py
+++ sixthings/views.py
@@ -1,6 +1,8 @@
 import datetime
-from django.views import generic
+from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseRedirect
+from django.views import generic
+from .forms import ThingCreateForm
 from .models import Thing
 
 class ThingListView(generic.ListView):
@@ -29,7 +31,7 @@ class ThingListView(generic.ListView):
             'name':   'tomorrow',
             'date':   tomorrow,
             'things': queryset.filter(date=tomorrow),
-        },]
+        }]
 
         for day in days:
             day['can_add'] = True if day['things'].count() < 6 else False
@@ -49,6 +51,8 @@ class ThingDoneView(ThingDeleteView):
         modified delete() to just mark as done
         """
         self.object = self.get_object()
+        if self.object.user != self.request.user:
+            raise PermissionDenied
         success_url = self.get_success_url()
         self.object.done = True
         self.object.save()
@@ -60,6 +64,8 @@ class ThingUndoneView(ThingDeleteView):
         modified delete() to just mark as undone
         """
         self.object = self.get_object()
+        if self.object.user != self.request.user:
+            raise PermissionDenied
         success_url = self.get_success_url()
         self.object.done = False
         self.object.save()
@@ -71,6 +77,8 @@ class ThingDeferView(ThingDeleteView):
         modified delete() to just copy to tomorrow
         """
         self.object = self.get_object()
+        if self.object.user != self.request.user:
+            raise PermissionDenied
         success_url = self.get_success_url()
         Thing.objects.create(
             user=self.object.user,
@@ -82,9 +90,10 @@ class ThingDeferView(ThingDeleteView):
 
 class ThingCreateView(generic.CreateView):
     model = Thing
-    fields = [
-        'user',
-        'date',
-        'text',
-    ]
+    form_class = ThingCreateForm
     success_url = '/'
+
+    def get_form_kwargs(self):
+        kwargs = super(ThingCreateView, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
