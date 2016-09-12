4bfcbad1e9e5c841c96a7948543420cd1e018ab1
Phillip Marshall <pmarshall@agrimgt.com>
2016-09-12 16:02:41 -0700

    added django-bootstrap3 and provided appropriate jinja bindings

--- project/jinja2.py
+++ project/jinja2.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.core.urlresolvers import reverse
+from bootstrap3.forms import render_formset, render_formset_errors, render_form, render_form_errors, render_field, render_label, render_button, render_icon
 from jinja2 import Environment
 
 
@@ -10,6 +11,14 @@ def environment(**options):
     env.globals.update({
         'static': staticfiles_storage.url,
         'url': reverse,
+        'bootstrap_formset':        render_formset,
+        'bootstrap_formset_errors': render_formset_errors,
+        'bootstrap_form':           render_form,
+        'bootstrap_form_errors':    render_form_errors,
+        'bootstrap_field':          render_field,
+        'bootstrap_label':          render_label,
+        'bootstrap_button':         render_button,
+        'bootstrap_icon':           render_icon,
     })
     return env
 
