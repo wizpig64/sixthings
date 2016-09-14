b5c32acdfe863d4b23bfc6dfb4b3e73954877f35
Phillip Marshall <pmarshall@agrimgt.com>
2016-09-13 17:55:04 -0700

    added media and static configuration

--- project/settings.py
+++ project/settings.py
@@ -154,10 +154,20 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Media files
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'serve/media')
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'serve/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 
 # Local settings
